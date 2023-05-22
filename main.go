24968727319327aafdf44ff5c9587e56f34a28fa
focta <focke.wulf7234@gmail.com>
2023-05-22 22:16:59 +0900

    jsonを返すための最小構成を実装

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+
+	articles := Articles{
+		Article {Title: "Test Title",Desc: "Test Description",Content: "Hello World",},
+}
+	fmt.Println("Endpoint Hit: All Article Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
 
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -13,6 +32,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
